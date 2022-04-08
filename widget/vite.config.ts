2cc71be1b77f9f2c8848dce10a5821ec1bae5187
Evert van der Weit <evert@mett.nl>
2022-04-08 22:05:55 +0200

    Widget and portal boilerplate

---
+++ widget/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue({ customElement: true })],
+  server: {
+    port: 4000,
+  },
+  build: {
+    lib: {
+      entry: path.resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        'src/main.ts'
+      ),
+      name: 'chat-widget',
+      fileName: (format) => `widget.${format}.js`,
+    },
+    outDir: '../dist/widget',
+  },
+});
