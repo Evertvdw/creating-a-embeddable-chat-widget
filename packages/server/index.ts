b0db6c56f583d0977b82b2f26115fced10a48b13
Evert van der Weit <evert@mett.nl>
2022-04-10 21:43:26 +0200

    Add socketIO to server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,9 +1,26 @@
 import express from 'express';
 import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [/http:\/\/localhost:\d*/],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(
+    `Socket ${socket.id} connected from origin: ${socket.handshake.headers.origin}`
+  );
+  socket.onAny((event, ...args) => {
+    console.log(event, args);
+  });
+});
 
 server.listen(5000, () => {
   console.log(
